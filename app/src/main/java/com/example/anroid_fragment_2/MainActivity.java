427b42e6b3ad7bd4691a9a1733f1dfbf302dbc7a
Lazypanda0316 <uganmaharjan@gmail.com>
2025-03-04 09:03:55 +0545

    menu and pop up menu

--- app/src/main/java/com/example/anroid_fragment_2/MainActivity.java
+++ app/src/main/java/com/example/anroid_fragment_2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.anroid_fragment_2;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button b1=findViewById(R.id.pop1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu= new PopupMenu(MainActivity.this,v);
+                MenuInflater menuInflater=popupMenu.getMenuInflater();
+                menuInflater.inflate(R.menu.popup_menu,popupMenu.getMenu());
+                popupMenu.show();
 
-        First_Fragment firstFragment =new First_Fragment();
-        Second_Fragement secondFragement=new Second_Fragement();
-        getSupportFragmentManager().beginTransaction().replace(R.id.F1, firstFragment). commit();
+            }
+        });
+//        First_Fragment firstFragment =new First_Fragment();
+//        Second_Fragement secondFragement=new Second_Fragement();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.F1, firstFragment). commit();
 //        getSupportFragmentManager().beginTransaction().replace(R.id.F2, secondFragement). commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
 
     }
 }
